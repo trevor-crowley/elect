a599b7fad84c21a73e3b08b4626a02f35e43b282
trevor-crowley <trevor_crowley@hotmail.com>
2020-01-05 21:07:08 -0500

    opt

--- mandelbrot/matlab/mandel_test.m
+++ mandelbrot/matlab/mandel_test.m
@@ -9,15 +9,19 @@ width = 0.1;
 grid = 1024;
 iter = 256;
 
+%% build
 buildInstrumentedMex mandel_fi ...
     -args {centre,width,grid,iter } -histogram
+%%
 
 [z_expected, c_expected] = mandel(centre,width,grid,iter);
+
+%% test
 [z, c] = mandel_fi_mex(centre,width,grid,iter);
+%%
 
-
-showInstrumentationResults mandel_fi_mex ...
-     -proposeFL defaultDT 'embedded.numerictype'
+% showInstrumentationResults mandel_fi_mex ...
+%      -proposeFL defaultDT 'embedded.numerictype'
  
 err = c - c_expected;
 
