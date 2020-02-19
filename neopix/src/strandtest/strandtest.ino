7545203969624f006430f239ab6bf431130c3774
trevor <trevor.crowley@henryschein.ca>
2020-02-18 19:26:11 -0500

    test 4 strand

--- neopix/src/strandtest/strandtest.ino
+++ neopix/src/strandtest/strandtest.ino
@@ -20,7 +20,7 @@
 #define LED_PIN    6
 
 // How many NeoPixels are attached to the Arduino?
-#define LED_COUNT 100
+#define LED_COUNT 4
 
 // Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
