1fb534476641755ca2aed4afe2d254d9962f0f2e
trevor-crowley <trevor_crowley@hotmail.com>
2021-04-16 10:54:21 -0400

    mac sync

---
+++ test/test/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  test
+//
+//  Created by trevor on 2020-10-12.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
